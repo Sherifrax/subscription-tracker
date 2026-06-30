4a60819da90613f294eea4d2bb0f3ea402ac057e
Abdalrahman Sherif <sherifrax@Abdalrahmans-MacBook-Air.local>
2026-06-30 07:03:50 +0400

    Database (mongoDb and mongoose) setup + ODM

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import {PORT} from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
@@ -17,8 +18,10 @@ app.get('/', (req,res) => {
 })
 
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
     console.log(`Server is running on port ${PORT}`);
+
+    await connectToDatabase()
 });
 
 export default app;
